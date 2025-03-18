3db2898757324337abfefa9cca1eab75c6105138
bansalkomal <bansalkomal93@gmail.com>
2025-03-19 04:42:21 +0530

    some changes

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -430,7 +430,7 @@ setActivePage("home")}>Home</button>
             ))};
 
             <li className="nav-item">
-              <button className="nav-link text-white" onClick={() => setActivePage("blog")}>Blogs</button>
+              <button className="nav-link text-white" onClick={() => setActivePage("blog")}>Blog</button>
             </li>
             <li className="nav-item">
               <button className="nav-link text-white" onClick={() => setActivePage("contactus")}>Contact Us</button>
